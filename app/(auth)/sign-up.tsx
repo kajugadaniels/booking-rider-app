4ea9ae08bf03fe8f4bcdfdcbaf473e8900645b54
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-23 02:32:03 +0200

    Add InputField component

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { icons, images } from '@/constants'
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="absolute text-2xl text-black font-JakartaSemiBold bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
